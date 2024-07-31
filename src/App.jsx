bf9973a5f7782313b70d18e818d770043abc3b23
Freework-site <amstv2223@gmail.com>
2024-07-31 03:23:30 -0600

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import AuthPage from './AuthPage'
+import ChatsPage from './ChatsPage'
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(undefined)
 
-  return (
-    <>
-      <h1>Vite React App started</h1>
-    </>
-  )
+  if (!user) {
+    return <AuthPage onAuth={user => setUser(user)} />
+  } else {
+    return <ChatsPage user={user} />
+  }
 }
 
 export default App
